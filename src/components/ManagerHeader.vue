504f572331d9e7b1d8a777c495213da87e64e1e6
djfksdl <djfksdl@naver.com>
2024-05-09 17:48:49 +0900

    헤더 틀

--- src/components/ManagerHeader.vue
+++ src/components/ManagerHeader.vue
@@ -1,12 +1,27 @@
 <template>
-    <div class="header">
-        <img class="logo" src="../assets/images/logo.png">
+    <div class="mheader">
+        <div class="mheaderWrap">
+            <!-- 로고 -->
+            <div class="mlogo">
+                <p>로고자리</p>
+            </div>
+            <!-- 메뉴 -->
+            <ul class="mMenu">
+                <!-- 회원+원장 -->
+                <router-link to=""><li>후기게시판</li></router-link>
+                <!-- 원장 로그인했을때 보이는것 -->
+                <router-link to=""><li>알림창</li></router-link>
+                <router-link to=""><li>예약스케쥴</li></router-link>
+                <router-link to=""><li>매출/통계</li></router-link>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import "@/assets/css/mheader/mheader.css"
 export default {
-    name: "AppHeader",
+    name: "ManagerHeader",
     components: {},
     data() {
         return {};
@@ -19,15 +34,5 @@ export default {
 </script>
 
 <style>
-.header{
-    width: 100%;
-    height: 138px;
-    background-color: #d6d6d6;
-}
 
-.logo{
-    width: 250px;
-    height: 150px;
-    object-fit: contain;
-}
 </style>
--- src/views/edit/EditView.vue
+++ src/views/edit/EditView.vue
@@ -1,20 +1,26 @@
 <template>
     <div id="wrap">
+        <ManagerHeader />
           <div class="container">
 
           </div>
            
-               
+        <ManagerFooter />  
        </div><!-- wrap --> 
+
   </template>
   
   
   <script>
-
+  
+   import ManagerFooter from "@/components/ManagerFooter.vue"
+   import ManagerHeader from "@/components/ManagerHeader.vue"
   
    export default {
        name: "EditView",
        components: {
+            ManagerFooter,
+            ManagerHeader
        },
        data() {
            return {
