4531f80807032f5adc1e69cc1310cae786569970
ojiwon99 <dhwldnjs0420@naver.com>
2024-05-10 16:38:17 +0900

    지원totalsales

---
+++ src/views/manager/TotalSalesView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- //html -->
+    </div>
+</template>
+
+
+<script>
+
+export default {
+        name: "TotalSalesView",
+        components: {          
+        },
+        data() {
+            return {
+                
+            };
+        },
+        methods: {
+           
+        },
+    };
+</script>
