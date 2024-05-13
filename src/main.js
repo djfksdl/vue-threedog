006315851f64c023ca726d00c7613dcc0ab82970
aassdd236 <aassdd236@naver.com>
2024-05-13 12:40:21 +0900

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from "./store/storage"
 
-createApp(App)
+createApp(App).use(router)
     .use(store)
     .use(router)
     .mount('#app')
