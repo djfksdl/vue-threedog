7f07cf0fa6d5dc9819a6ae172ebe9566628b6f24
djfksdl <djfksdl@naver.com>
2024-05-14 11:18:37 +0900

    ㅇㅅ 탑버튼 동그랗게, edit 슬라이드,맵 오류 수정

--- src/components/TopButton.vue
+++ src/components/TopButton.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <router-link to="#" class="toScrollButton" v-on:click="scrollToTop" :class="{ 'show': showButton }">
+            <!-- :class(디렉티브) :showButton이 true일때 show를 추가하고 flase일때는 show클래스를 제거 -->
             <div class="arrowUp"></div>
         </router-link>
     </div>
@@ -23,8 +24,8 @@ export default {
             });
         },
         handleScroll(){
-            //현재 스크롤 위치 확인
-            const scrollPosition = window.pageYOffset || document.documentElement.scrollTop; //현재 스크롤 위치를 가져오는 2가지인데, 둘중 하나라도 존재하면 변수에 할당됨. 2가지 차이는 브라우저마다 작용하는게 달라서(크로스브라우징)
+            //현재 스크롤 위치 확인 //현재 스크롤 위치를 가져오는 2가지방법인데, 둘중 하나라도 존재하면 변수에 할당됨. 2가지 차이는 브라우저마다 작용하는게 달라서(크로스브라우징)
+            const scrollPosition = window.pageYOffset || document.documentElement.scrollTop; 
 
             //일정 높이 이상 스크롤 되었는지 확인
             if(scrollPosition > 200){
