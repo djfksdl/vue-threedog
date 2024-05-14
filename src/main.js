782645f219524390372b6c1e49b1898dfa94ef50
aassdd236 <aassdd236@naver.com>
2024-05-14 16:24:04 +0900

    asd

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from "./store/storage"
 import { useKakao } from 'vue3-kakao-maps/@utils';
 
 window.Kakao.init("c0e61a675067767007fc527e42495678");
-useKakao('57e9df8ad65dc994e5c28c552418428b');
+useKakao('c0e61a675067767007fc527e42495678');
 createApp(App).use(router)
     .use(store)
     .use(router)
