1f0ea9bf3736be3496485bec6aaa8624d8ed1acc
ojiwon99 <dhwldnjs0420@naver.com>
2024-05-14 15:50:20 +0900

    지원이

---
+++ src/views/manager/ScheduleView.vue
@@ -0,0 +1,26 @@
+<template>
+    
+    <div>
+        <!-- //html -->
+    </div>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: "ScheduleView", // 컴포넌트 이름
+
+        components: {          
+        },
+        data() {
+            return {
+                
+            };
+        },
+        methods: {
+           
+        },
+    };
+</script>
