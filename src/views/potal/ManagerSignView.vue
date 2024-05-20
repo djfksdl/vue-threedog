c1efb2b6f03a844d5fe31fd2d7f7c1d42e26313b
aassdd236 <aassdd236@naver.com>
2024-05-20 11:38:10 +0900

    회원가입 + 아이디 중복 확인

--- src/views/potal/ManagerSignView.vue
+++ src/views/potal/ManagerSignView.vue
@@ -9,10 +9,12 @@
                     <form @submit.prevent="handleSubmit">
                         <div class="form-group-man">
                             <div class="input-container">
-                                <input type="text" id="id" name="id" placeholder="아이디">
+                                <input type="text" id="id" name="id" placeholder="아이디" v-model="id">
                                 <button type="button" @click="checkDuplicate" class="small-btn green-btn">중복확인</button>
                             </div>
-                            <small v-if="isDuplicate" class="error-message">중복된 아이디입니다.</small>
+                            <small v-if="isDuplicate === true" class="error-message">중복된 아이디입니다.</small>
+                            <small v-if="isDuplicate === false" class="success-message">가입 가능한 아이디입니다.</small>
+
                         </div>
                         <div class="form-group-man">
                             <input type="password" id="password" name="password" placeholder="비밀번호" v-model="password">
@@ -30,12 +32,13 @@
                         <div class="form-group-man">
                             <div class="input-container">
                                 <input type="text" id="num" name="num" placeholder="사업자등록번호" v-model="businessNumber">
-                                <button type="button" @click="checkBusinessNumber" class="small-btn green-btn">확인</button>
+                                <button type="button" @click="checkBusinessNumber"
+                                    class="small-btn green-btn">확인</button>
                             </div>
                             <small v-if="businessCheckMessage" class="error-message">{{ businessCheckMessage }}</small>
                         </div>
                         <div class="form-group-man">
-                            <input type="tel" id="storeNumber" name="storeNumber" placeholder="사업장 전화번호">
+                            <input type="tel" id="bPhone" name="bPhone" placeholder="사업장 전화번호" v-model="bPhone">
                         </div>
                         <div class="form-group" style="margin-bottom: 10px;">
                             <div class="input-container">
@@ -50,7 +53,8 @@
                         <div class="form-group" style="margin-bottom: 10px;">
                             <input type="text" v-model="detailAddress" placeholder="상세 주소">
                         </div>
-                        <button type="submit" class="msignupBtn">회원가입</button>
+                        <button type="submit" class="msignupBtn" :disabled="isDuplicate !== false">회원가입</button>
+                        <small v-if="isDuplicate === null" class="warning-message">아이디 중복을 확인해 주세요.</small>
                     </form>
                 </div>
             </div><!-- signup-box -->
@@ -76,7 +80,8 @@ export default {
     data() {
         return {
             showVerificationInput: false, // 인증번호 입력 상자 보이기 여부
-            isDuplicate: false, // 아이디 중복 여부
+            isDuplicate: null, // 아이디 중복 여부
+            password: "", // 비밀번호
             confirmPassword: "", // 비밀번호 확인 값
             isPasswordMatch: true, // 비밀번호 일치 여부
             isPasswordValid: false, // 사용 가능한 비밀번호 여부
@@ -90,10 +95,50 @@ export default {
     methods: {
         handleSubmit() {
             console.log("회원가입");
+
+            const formData = new FormData();
+            formData.append("bId", this.id);
+            formData.append("bPw", this.password);
+            formData.append("bNum", this.businessNumber);
+            formData.append("bZipCode", this.zonecode);
+            formData.append("bAddress", this.roadAddress);
+            formData.append("bdAddress", this.detailAddress);
+            formData.append("bPhone", this.bPhone);
+
+            axios({
+                method: 'post', // put, post, delete 
+                url: `${this.$store.state.apiBaseUrl}/api/msignup`,
+                headers: { "Content-Type": "multipart/form-data" },
+                data: formData, // put, post, delete 방식 자동으로 JSON으로 변환 전달
+                responseType: 'json' // 수신타입
+            }).then(response => {
+                console.log(response.data);
+                this.$router.push('/mlogin');
+
+            }).catch(error => {
+                console.log(error);
+            });
         },
-        checkDuplicate() {
-            console.log("아이디 중복확인");
-            this.isDuplicate = true;
+        async checkDuplicate() {
+
+            axios({
+                method: 'get', // put, post, delete 
+                url: `${this.$store.state.apiBaseUrl}/api/msignup/id`,
+                headers: { "Content-Type": "multipart/form-data" },
+                params: {
+                    id: this.id
+                },
+            }).then(response => {
+                console.log(response.data);
+                if (response.data.apiData == 1) {
+                    this.isDuplicate = true;
+                } else {
+                    this.isDuplicate = false;
+                }
+
+            }).catch(error => {
+                console.log(error);
+            });
         },
         checkPasswordMatch() {
             this.isPasswordMatch = this.password === this.confirmPassword;
@@ -108,34 +153,34 @@ export default {
             }).open();
         },
         async checkBusinessNumber() {
-    try {
-        const response = await axios.post('https://api.url/endpoint', {
-            b_no: [this.businessNumber]
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': 'Bearer kLwu8It5iiIWVEWui/FpNx7qI2XcPU6H6lfgnHJ1RGVI0nNAR9yfRk7eWA8m9ncjMV/SeJ2g36xCarutBsixGw==' // 여기에 서비스 키 추가
-            }
-        });
+            try {
+                const response = await axios.post('https://bizno.net/api/fapi', {
+                    b_no: [this.businessNumber]
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer kLwu8It5iiIWVEWui/FpNx7qI2XcPU6H6lfgnHJ1RGVI0nNAR9yfRk7eWA8m9ncjMV/SeJ2g36xCarutBsixGw==' // 여기에 서비스 키 추가
+                    }
+                });
 
-        console.log('Response:', response.data); // 응답 데이터 로그
-        if (response.data && response.data.data && response.data.data.length > 0) {
-            const valid = response.data.data[0].b_stt_cd;
-            if (valid === '01') {
-                this.businessCheckMessage = "사업자 회원가입이 가능합니다.";
-            } else {
-                this.businessCheckMessage = "사업자 회원가입을 할 수 없습니다.";
+                console.log('Response:', response.data); // 응답 데이터 로그
+                if (response.data && response.data.data && response.data.data.length > 0) {
+                    const valid = response.data.data[0].b_stt_cd;
+                    if (valid === '01') {
+                        this.businessCheckMessage = "사업자 회원가입이 가능합니다.";
+                    } else {
+                        this.businessCheckMessage = "사업자 회원가입을 할 수 없습니다.";
+                    }
+                } else {
+                    this.businessCheckMessage = "올바른 번호를 입력해 주세요.";
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                this.businessCheckMessage = "올바른 번호를 입력해 주세요.";
             }
-        } else {
-            this.businessCheckMessage = "올바른 번호를 입력해 주세요.";
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        this.businessCheckMessage = "올바른 번호를 입력해 주세요.";
-    }
+        },
+    },
 }
-
-    }
-};
 </script>
+
